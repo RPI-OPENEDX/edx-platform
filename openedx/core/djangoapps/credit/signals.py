b5237b33522d6df6a61437c054a0e81e6e9406e3
Awais <awais.qureshi@arbisoft.com>
2015-06-22 14:38:19 +0500

    ECOM-1597 adding signals to update min-grade requirement.

--- openedx/core/djangoapps/credit/signals.py
+++ openedx/core/djangoapps/credit/signals.py
@@ -3,8 +3,11 @@ This file contains receivers of course publication signals.
 """
 
 from django.dispatch import receiver
+from django.utils import timezone
+from opaque_keys.edx.keys import CourseKey
 
 from xmodule.modulestore.django import SignalHandler
+from courseware.grades import MIN_GRADE_REQUIREMENT_STATUS
 
 
 @receiver(SignalHandler.course_published)
@@ -18,3 +21,41 @@ def listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=
     from .tasks import update_credit_course_requirements
 
     update_credit_course_requirements.delay(unicode(course_key))
+
+
+@receiver(MIN_GRADE_REQUIREMENT_STATUS)
+def listen_for_grade_calculation(sender, username, grade_summary, course_key, deadline, **kwargs):  # pylint: disable=unused-argument
+    """Receive 'MIN_GRADE_REQUIREMENT_STATUS' signal and update minimum grade
+    requirement status.
+
+    Args:
+        sender: None
+        username(string): user name
+        grade_summary(dict): Dict containing output from the course grader
+        course_key(CourseKey): The key for the course
+        deadline(datetime): Course end date or None
+
+    Kwargs:
+        kwargs : None
+
+    """
+    from openedx.core.djangoapps.credit.api import (
+        is_credit_course, get_credit_requirement, set_credit_requirement_status
+    )
+
+    course_key = CourseKey.from_string(unicode(course_key))
+    is_credit = is_credit_course(course_key)
+    if is_credit:
+        requirement = get_credit_requirement(course_key, 'grade', 'grade')
+        if requirement:
+            criteria = requirement.get('criteria')
+            if criteria:
+                min_grade = criteria.get('min_grade')
+                if grade_summary['percent'] >= min_grade:
+                    set_credit_requirement_status(
+                        username, course_key, 'grade', 'grade', status="satisfied", reason=grade_summary['percent']
+                    )
+                elif deadline and deadline < timezone.now():
+                    set_credit_requirement_status(
+                        username, course_key, 'grade', 'grade', status="failed", reason={}
+                    )
